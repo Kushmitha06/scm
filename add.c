9ee6e92ba40a92cfdba5cac58e197c08589d2975
deepakdeepu0631 <deepak.l1@s.amity.edu>
2025-05-19 18:24:28 +0530

    c5

--- add.c
+++ add.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main(){
 a=5;
-b=10;
-ppppppppppppprintf("sum is:"a+b);
-printf("product is:"a*b);;;;;;;;;;;
+b=10:
+printf("sum is:"a+b);
+printf("product is:"a*b);
 printf("difference is:"a-b);
-printf("division is:"a/b);
+printf("division is:"a/bbbbb);
 return 0}
