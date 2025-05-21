7dfd1c141306b3d7f6459fab560a87d7c75b48cd
Kunal S Chandnani <kunal.chandnani@s.amity.edu>
2025-05-21 11:28:24 +0530

    Deleted everything except addition

--- add.c
+++ add.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
 void main(){
 a=5;
-b=10;
-printf("sum is:"a+b);
-printf("product is:"a*b);
-printf("difference is:"a-b);
-printf("division is:"a/bbbbbbbb);
 b=10:
 printf("sum is:"a+b);
-printf("product is:"a*b);
-printf("difference is:"a-b);
-printf("division is:"a/bbbbb);
 return 0}
